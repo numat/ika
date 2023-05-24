649078f3fb94da3de08547432483d97ada2a4931
Alex Ruddick <a.ruddick@numat-tech.com>
2023-05-24 13:30:15 -0500

    Bump version for bugfix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as in_file:
 
 setup(
     name="ika-control",
-    version="0.2.0",
+    version="0.2.1",
     description="Python driver for IKA instruments.",
     long_description=long_description,
     long_description_content_type='text/markdown',
